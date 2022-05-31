3f3456e3c2ef5fb6bb903be7647b9e30f2e362a1
1977953215 <1977953215@qq.com>
2022-05-31 15:17:54 +0800

    feat:华豫云-智慧工地App-选择默认组件

--- src/api/request.js
+++ src/api/request.js
@@ -4,9 +4,9 @@ import qs from "querystringify";
 let apiContextPath = "";
 if (process.env.NODE_ENV === "development") {
   document.cookie =
-    "token=eyJhbGciOiJIUzI1NiJ9.eyJsb2dpblRpbWVzdGFtcCI6MTY0NjcyMjI2ODY4NSwidXNlcklkIjoiMTIzNDU2Nzg5MCJ9.F8wr84ha-dW18J9wZOQeTXj55mXTdqKfLBeNlNueoLY";
+    "token=eyJhbGciOiJIUzI1NiJ9.eyJsb2dpblRpbWVzdGFtcCI6MTY1Mzk2NDA2MTcxOCwidXNlcklkIjoiMTIzNDU2Nzg5MCJ9._7pnOeKzCwr9SzxUNnGlDJOwXGPQKkgWy69bAoZtu40";
   document.cookie =
-    "refreshToken=eyJhbGciOiJIUzI1NiJ9.eyJsb2dpblRpbWVzdGFtcCI6MTY0NjcyMjI2ODY4Nn0.TEVE_nopHNZlvSQM_RUZrLcCzkaERiHo8nz0q-ksL3E";
+    "refreshToken=eyJhbGciOiJIUzI1NiJ9.eyJsb2dpblRpbWVzdGFtcCI6MTY1Mzk2NDA2MTcxOX0.hnN_6dbiRgghBAfCm69LkTmssvyBLXO7ENPNPTEHZAs";
   document.cookie = "username=admin";
   document.cookie = "windowOnline=true";
   apiContextPath = "/api";
